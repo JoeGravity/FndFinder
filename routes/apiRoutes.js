6147dc6093c9793ff176f78843072041d266f68d
Joe Pfahl <joegravity@gmail.com>
2018-04-12 18:50:21 -0500

    minor tweaks and sync

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,12 +19,13 @@ module.exports = function (app) {
 
   app.get("/api/friends", function (req, res) {
     res.json(friends);
+    // console.log(res);
   });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
+  // ...the  JSON is pushed to the appropriate JavaScript array
 
   // (ex. User fills out survey ... this data is then sent to the server...
   // Then the server saves the data to the friends array)
